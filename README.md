2bd462c257cc8d81f82521b679b9572ef068ffed
Andrés Fernández <anfernandez@fi.uba.ar>
2023-10-27 23:28:45 -0300

    adds tests for game creation exceptions

--- README.md
+++ README.md
@@ -39,3 +39,5 @@ docker compose -f local.yml run --rm --service-ports django.tic_tac_toe_api
 * Este cambio me llevó algo de tiempo, hubiera sido más simple tener player_1 y player_2 dentro de Game, pero me pareció claro que no era la idea del enunciado. Por otro lado, la adición de los jugadores al juego no puede hacerse en la creación, por limitaciones de Django, por lo que quedó en un método aparte, y llevó a permitir algunos campos en null=True (Person->game y Game->next_move). También podía overridear el delete de Game para que borre los players, pero me pareció mejor esta alternativa.
 
 * Next move podría ser simplemente un booleano (0,1) o int que sirviera para moverse en el array de jugadores (1 o 2). Sin embargo, lo defino como una foreign key a Player que es simplemente un id y funciona de forma similar.
+
+* Agrego validación de que nombres y símbolos de jugadores no sean iguales para el mismo juego tanto a nivel de serializer como de modelo. Si bien obviamente esto implica una redundancia en la validación, entiendo que el modelo debe verificarlo y es una validación muy simple.
--- tic_tac_toe_api/tests/views/test_game_creation.py
+++ tic_tac_toe_api/tests/views/test_game_creation.py
@@ -49,3 +49,60 @@ class GameCreationTestCase(APITestCase):
         self.assertEqual(player_1["symbol"], PLAYER_1_SYMBOL)
         self.assertEqual(player_2["name"], "Player 2")
         self.assertEqual(player_2["symbol"], PLAYER_2_SYMBOL)
+
+    def test_game_creation_with_extra_players_throws_exception(self):
+        payload = {
+            "players": [
+                {"name": "Player 1"},
+                {"name": "Player 2"},
+                {"name": "Player 3"},
+            ],
+            "starting_player": "Player 1",
+        }
+        url = "/games/"
+        response = self.client.post(url, payload, format="json")
+        self.assertEqual(response.status_code, 400)
+        response_content = str(response.json())
+        self.assertIn("A game must have exactly two players", response_content)
+
+    def test_game_creation_with_incorrect_starting_player_throws_exception(self):
+        payload = {
+            "players": [
+                {"name": "Player 1"},
+                {"name": "Player 2"},
+            ],
+            "starting_player": "Player 3",
+        }
+        url = "/games/"
+        response = self.client.post(url, payload, format="json")
+        self.assertEqual(response.status_code, 400)
+        response_content = str(response.json())
+        self.assertIn("Starting player must be one of the players", response_content)
+
+    def test_game_creation_with_same_player_names_throws_exception(self):
+        payload = {
+            "players": [
+                {"name": "Player 1"},
+                {"name": "Player 1"},
+            ],
+            "starting_player": "Player 1",
+        }
+        url = "/games/"
+        response = self.client.post(url, payload, format="json")
+        self.assertEqual(response.status_code, 400)
+        response_content = str(response.json())
+        self.assertIn("Players must have different names", response_content)
+
+    def test_game_creation_with_same_player_symbols_throws_exception(self):
+        payload = {
+            "players": [
+                {"name": "Player 1", "symbol": "X"},
+                {"name": "Player 2", "symbol": "X"},
+            ],
+            "starting_player": "Player 1",
+        }
+        url = "/games/"
+        response = self.client.post(url, payload, format="json")
+        self.assertEqual(response.status_code, 400)
+        response_content = str(response.json())
+        self.assertIn("Players must have different symbols", response_content)
